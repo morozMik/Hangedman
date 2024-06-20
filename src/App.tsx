3991ad42603e66dfd17b12d4eedba358f5fda6ca
morozMyk <mik.moroz0387@gmail.com>
2024-06-20 13:53:43 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+function App() {
+
+  const [wordToGuess, setWordToGuess] = useState('Test')
+
+
+  return (
+    <>
+      <h1>
+        Hello
+      </h1>
+      <div>
+        asdasddassasasasasasasasasa
+      </div>
+    </>
+  )
+}
+
+export default App
